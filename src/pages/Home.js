ea8a19d381fb6b9323a3840dd7d89f36bb24b4e1
Frédéric <desseaux_frederic@yahoo.fr>
2021-07-20 22:36:57 +0200

    map page Home ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <Link to="/offer">Offer</Link>
-    </div>
+  const [offers, setOffers] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://lereacteur-vinted-api.herokuapp.com/offers"
+      );
+      setOffers(response.data.offers);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement...</span>
+  ) : (
+    <main className="container">
+      {offers.map((offer, index) => (
+        <Link to={`/offer/${offer._id}`} key={offers._id}>
+          <div>
+            <div className="card-container">
+              <div className="card-username">
+                <span>{offer.owner.account.username}</span>
+              </div>
+              <div>
+                <img
+                  className="card-img"
+                  src={offer.product_image.url}
+                  alt={`${offer._id}`}
+                />
+                <div className="card-infos">
+                  <span>{offer.product_price}</span>
+                  <span>{offer.product_details[1].TAILLE}</span>
+                  <span>{offer.product_details[0].MARQUE}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </main>
   );
 };
 
--- src/pages/Offer.js
+++ src/pages/Offer.js
@@ -1,10 +1,35 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Offer = () => {
+  const { id } = useParams();
+
+  const [offer, setOffer] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(
+        `https://lereacteur-vinted-api.herokuapp.com/offer/${id}`
+      );
+      setOffer(result.data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <Link to="/">Home</Link>
+      Hello {id}
+      {isLoading ? (
+        <div>En cours de chargement...</div>
+      ) : (
+        <div>
+          <img src={offer.product_image.secure_url} alt={offer.product_name} />
+        </div>
+      )}
     </div>
   );
 };
