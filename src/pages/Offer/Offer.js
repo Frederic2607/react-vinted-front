055f24043257b7bcc0ec63f747811d2a070f2399
Frédéric <desseaux_frederic@yahoo.fr>
2021-08-06 10:50:57 +0200

    offer-page css ok

--- src/pages/Offer/Offer.js
+++ src/pages/Offer/Offer.js
@@ -1,3 +1,5 @@
+import "./offer.css";
+
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
@@ -27,37 +29,40 @@ const Offer = () => {
       ) : (
         <div className="offer-page">
           <div className="offer-container">
-            <div className="offer-picture">
+            <div className="offer-pictures">
               <img
+                className="offer-picture"
                 src={offer.product_image.secure_url}
                 alt={offer.product_name}
               />
             </div>
             <div className="offer-infos">
               <div>
-                {offer.product_price}
-                {offer.product_details.map((detail, index) => {
-                  console.log(detail);
-
-                  const keys = Object.keys(detail);
-
-                  return (
-                    <div key={index}>
-                      {keys[0]}: {detail[keys[0]]}
-                    </div>
-                  );
-                })}
+                <span className="offer-price">{offer.product_price} €</span>
+                <ul className="offer-list">
+                  {offer.product_details.map((detail, index) => {
+                    const keys = Object.keys(detail);
+                    return (
+                      <li key={index} className="offer-list-infos">
+                        <span>{keys[0]}</span>
+                        <span>{detail[keys[0]]}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
               </div>
-              <div className="divider"></div>
+              <div className="split"></div>
               <div className="offer-content">
-                {offer.product_name}
-                {offer.product_description}
-                {offer.owner.account.username}
+                <p className="offer-name">{offer.product_name}</p>
+                <p className="offer-description">{offer.product_description}</p>
+                <div className="offer-avatar">
+                  <span>{offer.owner.account.username}</span>
+                </div>
               </div>
+              <Link to="/payment">
+                <button>Acheter</button>
+              </Link>
             </div>
-            <Link to="/payment">
-              <button>Acheter</button>
-            </Link>
           </div>
         </div>
       )}
